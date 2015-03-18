90bd8df8892dc8f26e40097ce735620d0fbade50
jkopczyn <ja.kopczynski@gmail.com>
2015-03-18 15:55:28 -0700

    Working server rendering templates and JSON on different pages

--- bin/pH_server.rb
+++ bin/pH_server.rb
@@ -6,8 +6,10 @@ require_relative 'classes.rb'
 
 router = Phase6::Router.new
 router.draw do
-  get Regexp.new("^/cats$"), Cats2Controller, :index
+  get Regexp.new("^/cats$"), CatsController, :index
   get Regexp.new("^/cats/(?<cat_id>\\d+)/statuses$"), StatusesController, :index
+  get Regexp.new("^/?$"), CatsController, :index
+  get Regexp.new("^/cat/(?<id>\\d+)$"), CatsController, :show
 end
 
 server = WEBrick::HTTPServer.new(Port: 3000)
