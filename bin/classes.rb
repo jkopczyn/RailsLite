9309abca171152ff5cc801b0a6a6eefad9b15078
Jacob Kopczynski <ja.kopczynski@gmail.com>
2015-03-18 15:05:15 -0700

    Working server with minimal db, models, controller, and views

--- bin/classes.rb
+++ bin/classes.rb
@@ -27,43 +27,14 @@ class Cat < SQLObject
   finalize!
 end
 
-$cats = [
-  { id: 1, name: "Curie" },
-  { id: 2, name: "Markov" }
-]
-
-$statuses = [
-  { id: 1, cat_id: 1, text: "Curie loves string!" },
-  { id: 2, cat_id: 2, text: "Markov is mighty!" },
-  { id: 3, cat_id: 1, text: "Curie is cool!" }
-]
-
 class StatusesController < PhaseHigher::ControllerBase
-  def initialize(*args)
-    super
-    $statuses.each do |status|
-      Status.new(status).save
-    end
-  end
-
   def index
-    statuses = $statuses.select do |s|
-      s[:cat_id] == Integer(params[:cat_id])
-    end
-
-    render_content(statuses.to_s, "text/text")
+    render_content(Status.all.to_json, "text/json")
   end
 end
 
 class Cats2Controller < PhaseHigher::ControllerBase
-  def initialize(*args)
-    super
-    $cats.each do |cat|
-      Status.new(cat).save
-    end
-  end
-  
   def index
-    render_content($cats.to_s, "text/text")
+    render_content(Cat.all.to_json, "text/json")
   end
 end
